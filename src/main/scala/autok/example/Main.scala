83ef3858b320baf5d9d9897aabe75a92693c5cbc
Dmytro Starosud <dmytro.starosud@captify.co.uk>
2015-05-15 16:09:22 +0300

    timer should be daemon

--- src/main/scala/autok/example/Main.scala
+++ src/main/scala/autok/example/Main.scala
@@ -1,33 +1,52 @@
 package autok.example
 
-import java.util.{ TimerTask, Timer }
+import java.util.{ Timer, TimerTask }
 
 import autok.authentication.{ Authentication, SimpleAuthentication, StubAuthServerConfig, Token }
 import autok.mocks.LocalHostTokenServer
 import com.github.tomakehurst.wiremock.core.Container
 
 import scala.annotation.tailrec
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{ Await, blocking }
 import scala.concurrent.duration.Duration
+import scala.concurrent.{ Await, blocking }
 import scala.io.StdIn
-import scala.util.Try
-import scalaj.http.Http
+import scala.util.{ Success, Try }
+import scalaj.http.{ Http, HttpResponse }
 
 case class Mock(containers: Seq[Container])
 
 object Main extends App {
   @tailrec
-  final def loop(token: Token): Unit = {
-    println()
-    print("URL> ")
-    val url = StdIn.readLine().trim
-    if (url.nonEmpty) {
-      val result = Try(Http(url)
+  final def loop(tokenOpt: Option[Token], currentRequest: Option[String]): Unit = {
+    val token = tokenOpt.getOrElse {
+      blocking {
+        Await.result(authService.getToken, Duration.Inf)
+      }
+    }
+
+    val request = currentRequest.getOrElse {
+      println()
+      print("URL> ")
+      StdIn.readLine().trim
+    }
+
+    if (request.nonEmpty) {
+      val result = Try(Http(request)
         .header("Authorization", token)
         .asString)
       println(result)
-      loop(token)
+
+      val tokenExpired = result match {
+        case Success(HttpResponse(_, 401, _)) => true
+        case _ => false
+      }
+
+      if (tokenExpired) {
+        authService.tokenExpired(token)
+        loop(None, Some(request))
+      } else {
+        loop(Some(token), None)
+      }
     }
   }
 
@@ -46,22 +65,17 @@ object Main extends App {
             password = LocalHostTokenServer.PASS))
 
         // invalidate tokens every 10 seconds
-        (new Timer).schedule(new TimerTask {
+        new Timer(true).schedule(new TimerTask {
           override def run(): Unit = {
             LocalHostTokenServer.invalidateAllTokens()
           }
-        }, 10000)
+        }, 0, 10000)
 
         (service, mocks)
     }
 
-  val tokenFuture = authService.getToken
-  tokenFuture.failed.foreach(println)
-
   println("Enter empty line to exit")
-  blocking {
-    Await.ready(tokenFuture.map(loop), Duration.Inf)
-  }
+  loop(None, None)
 
   mocks.foreach(_.shutdown())
 }
